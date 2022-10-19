fa2539e3a41b1c320a592dc0f4369732c99b2c5c
sniederhumer <stefan.niederhumer@cloudflight.io>
2022-10-19 16:20:16 +0200

    SVCWEB-118 Eslint: Only allow fixed versions in package.json (#14)
    * SVCWEB-118 Eslint: Only allow fixed versions in package.json
    
    - Add new rule to enforce fixed versions in package.json dependencies and devDependencies
    - Add tests verifying the intended behavior
    
    Signed-off-by: Stefan Niederhumer <Stefan.Niederhumer@cloudflight.io>
    
    * SVCWEB-118 Eslint: Only allow fixed versions in package.json
    
    - Bump volta cli version to 4.0.0 to fix CI
    
    Signed-off-by: Stefan Niederhumer <Stefan.Niederhumer@cloudflight.io>
    
    Signed-off-by: Stefan Niederhumer <Stefan.Niederhumer@cloudflight.io>

---
+++ packages/eslint-plugin-typescript/src/rules/package/package-force-absolute-version-dependencies.rule.spec.ts
@@ -0,0 +1,106 @@
+import { RuleTester } from 'eslint';
+import { PackageForceAbsoluteVersionDependenciesRule } from './package-force-absolute-version-dependencies.rule';
+
+const ruleTester = new RuleTester({
+    parser: require.resolve('eslint-plugin-json-es'),
+});
+ruleTester.run('package-force-absolute-version-dependencies', PackageForceAbsoluteVersionDependenciesRule, {
+    valid: [
+        {
+            code: '{"dependencies": { "dependency": "1.0.0"}}',
+        },
+        {
+            code: '{"devDependencies": { "devDependency": "1.0.0"}}',
+        },
+        {
+            code: '{"dependencies": { "dependency": "1.0.0"}, "devDependencies": { "devDependency": "1.0.0"}}',
+        },
+        {
+            code: '{"dependencies": { "dependency": "http://asdf.com/asdf.tar.gz"}}',
+        },
+        {
+            code: '{"dependencies": { "dependency": "file:../dyl"}}',
+        },
+        {
+            code: '{"dependencies": { "dependency": "latest"}}',
+        },
+        {
+            code: '{"dependencies": { "dependency": "git+ssh://git@github.com:npm/cli.git"}}',
+        },
+        {
+            code: '{"dependencies": { "dependency": "user/repo#feature\\/branch"}}',
+        },
+    ],
+    invalid: [
+        {
+            code: '{"dependencies": { "dependency": ">1.0.0"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"devDependencies": { "dependency": ">1.0.0"}}',
+            output: '{"devDependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `devDependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": ">=1.0.0"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "<1.0.0"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "<=1.0.0"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "~1.0.0"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "^1.0.0"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "1.0.x"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "1.x.0"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "1.x.x"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "*"}}',
+            output: '{"dependencies": { "dependency": "*"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to an absolute version!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": ""}}',
+            output: '{"dependencies": { "dependency": ""}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to an absolute version!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "1.0.0 - 2.0.0"}}',
+            output: '{"dependencies": { "dependency": "1.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '1.0.0'!` }],
+        },
+        {
+            code: '{"dependencies": { "dependency": "4.0.0 || 1.0.0 - 2.0.0"}}',
+            output: '{"dependencies": { "dependency": "4.0.0"}}',
+            errors: [{ message: `dependencies option 'dependency' must be set to '4.0.0'!` }],
+        },
+    ],
+});
