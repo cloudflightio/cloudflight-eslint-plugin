33b888075754cd731ecb3fb73ed5396c38c18488
strawberry-choco <kangrui.ye@cloudflight.io>
2022-12-06 14:51:18 +0100

    Update dependencies (#51)
    * refactor rules
    
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>
    
    * update dependencies
    
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>
    
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

--- packages/eslint-plugin-typescript/src/rules/package/package-force-absolute-version-dependencies.rule.ts
+++ packages/eslint-plugin-typescript/src/rules/package/package-force-absolute-version-dependencies.rule.ts
@@ -10,37 +10,40 @@ export const PackageForceAbsoluteVersionDependenciesRule: Rule.RuleModule = {
                 const propertyPath = findPropertyPath(propertyNode);
                 const dependencyPath = propertyPath.match(/^\b(d|devD)ependencies\b/);
 
-                if (dependencyPath && propertyNode.value.type === 'Literal') {
-                    const propertyValue = JSON.stringify(propertyNode.value.value);
-                    const foundVersion = (propertyValue.match(semVerRegex) ?? [])[0];
-                    const isAnyVersion = propertyValue === JSON.stringify('*') || propertyValue === JSON.stringify('');
+                if (dependencyPath == null || propertyNode.value.type !== 'Literal') {
+                    return;
+                }
 
-                    if (foundVersion || isAnyVersion) {
-                        const dependencyContext = dependencyPath[0];
-                        const propertyName = getPropertyName(propertyNode);
+                const propertyValue = JSON.stringify(propertyNode.value.value);
+                const foundVersion = (propertyValue.match(semVerRegex) ?? [])[0];
+                const isAnyVersion = propertyValue === JSON.stringify('*') || propertyValue === JSON.stringify('');
 
-                        if (isAnyVersion && propertyName) {
-                            context.report({
-                                node: propertyNode,
-                                message: `${dependencyContext} option '${propertyName}' must be set to an absolute version!`,
-                            });
-                        } else if (foundVersion) {
-                            const isAbsoluteVersion =
-                                propertyValue.length === JSON.stringify(foundVersion).length &&
-                                !propertyValue.includes('x');
+                if (!(foundVersion || isAnyVersion)) {
+                    return;
+                }
 
-                            if (!isAbsoluteVersion && dependencyContext && propertyName) {
-                                reportWrongPropertyValue(
-                                    context,
-                                    propertyNode,
-                                    {
-                                        key: propertyName,
-                                        expectedValue: foundVersion.replace(/x/g, '0'),
-                                    },
-                                    dependencyContext
-                                );
-                            }
-                        }
+                const dependencyContext = dependencyPath[0];
+                const propertyName = getPropertyName(propertyNode);
+
+                if (isAnyVersion && propertyName != null && propertyName !== '') {
+                    context.report({
+                        node: propertyNode,
+                        message: `${dependencyContext} option '${propertyName}' must be set to an absolute version!`,
+                    });
+                } else if (foundVersion != null && foundVersion !== '') {
+                    const isAbsoluteVersion =
+                        propertyValue.length === JSON.stringify(foundVersion).length && !propertyValue.includes('x');
+
+                    if (!isAbsoluteVersion && dependencyContext && propertyName) {
+                        reportWrongPropertyValue(
+                            context,
+                            propertyNode,
+                            {
+                                key: propertyName,
+                                expectedValue: foundVersion.replace(/x/g, '0'),
+                            },
+                            dependencyContext
+                        );
                     }
                 }
             },
