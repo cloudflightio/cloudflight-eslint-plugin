b06e2444f85a7e98a1bdb882e90c2674031b0fcf
strawberry-choco <kangrui.ye@cloudflight.io>
2023-04-14 08:10:08 +0200

    enable more angular rules (#116)
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

--- packages/eslint-plugin-angular/src/configs/recommended-html/rules/angular-eslint-template.ts
+++ packages/eslint-plugin-angular/src/configs/recommended-html/rules/angular-eslint-template.ts
@@ -3,7 +3,14 @@ import { Linter } from 'eslint';
 const pluginPrefix = '@angular-eslint/template';
 
 export const angularTemplateEslintRules: Linter.RulesRecord = {
+    [`${pluginPrefix}/accessibility-alt-text`]: ['error'],
+    [`${pluginPrefix}/accessibility-elements-content`]: ['error'],
+    [`${pluginPrefix}/accessibility-interactive-supports-focus`]: ['error'],
+    [`${pluginPrefix}/accessibility-label-has-associated-control`]: ['error'],
+    [`${pluginPrefix}/accessibility-role-has-required-aria`]: ['error'],
+    [`${pluginPrefix}/accessibility-valid-aria`]: ['error'],
     [`${pluginPrefix}/banana-in-box`]: ['error'],
+    [`${pluginPrefix}/button-has-type`]: ['error'],
     [`${pluginPrefix}/eqeqeq`]: [
         'off', // disable it for now since it does not work correctly
         {
@@ -11,5 +18,10 @@ export const angularTemplateEslintRules: Linter.RulesRecord = {
         },
     ],
     [`${pluginPrefix}/no-any`]: ['error'],
+    [`${pluginPrefix}/no-call-expression`]: ['error'],
+    [`${pluginPrefix}/no-duplicate-attributes`]: ['error'],
+    [`${pluginPrefix}/no-inline-styles`]: ['error'],
+    [`${pluginPrefix}/no-interpolation-in-attributes`]: ['error'],
     [`${pluginPrefix}/no-negated-async`]: ['error'],
+    [`${pluginPrefix}/no-positive-tabindex`]: ['error'],
 };
