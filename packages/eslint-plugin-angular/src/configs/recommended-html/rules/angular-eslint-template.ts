77a858dedbaed9fcca9b42c3f97386d44621e90c
Kangrui Ye <kangrui.ye@cloudflight.io>
2023-06-01 08:41:05 +0200

    update dependencies
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

--- packages/eslint-plugin-angular/src/configs/recommended-html/rules/angular-eslint-template.ts
+++ packages/eslint-plugin-angular/src/configs/recommended-html/rules/angular-eslint-template.ts
@@ -3,20 +3,18 @@ import {Linter} from 'eslint';
 const pluginPrefix = '@angular-eslint/template';
 
 export const angularTemplateEslintRules: Linter.RulesRecord = {
-    [`${pluginPrefix}/accessibility-alt-text`]: ['error'],
-    [`${pluginPrefix}/accessibility-elements-content`]: ['error'],
-    [`${pluginPrefix}/accessibility-interactive-supports-focus`]: ['error'],
-    [`${pluginPrefix}/accessibility-label-has-associated-control`]: ['error'],
-    [`${pluginPrefix}/accessibility-role-has-required-aria`]: ['error'],
-    [`${pluginPrefix}/accessibility-valid-aria`]: ['error'],
+    [`${pluginPrefix}/alt-text`]: ['error'],
     [`${pluginPrefix}/banana-in-box`]: ['error'],
     [`${pluginPrefix}/button-has-type`]: ['error'],
+    [`${pluginPrefix}/elements-content`]: ['error'],
     [`${pluginPrefix}/eqeqeq`]: [
         'off', // disable it for now since it does not work correctly
         {
             allowNullOrUndefined: true,
         },
     ],
+    [`${pluginPrefix}/interactive-supports-focus`]: ['error'],
+    [`${pluginPrefix}/label-has-associated-control`]: ['error'],
     [`${pluginPrefix}/no-any`]: ['error'],
     [`${pluginPrefix}/no-call-expression`]: [
         'error',
@@ -30,4 +28,6 @@ export const angularTemplateEslintRules: Linter.RulesRecord = {
     [`${pluginPrefix}/no-interpolation-in-attributes`]: ['error'],
     [`${pluginPrefix}/no-negated-async`]: ['error'],
     [`${pluginPrefix}/no-positive-tabindex`]: ['error'],
+    [`${pluginPrefix}/role-has-required-aria`]: ['error'],
+    [`${pluginPrefix}/valid-aria`]: ['error'],
 };
