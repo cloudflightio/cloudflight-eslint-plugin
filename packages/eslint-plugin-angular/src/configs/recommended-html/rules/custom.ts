33a6f4214c73ec3d1c96692ea3ab71b456800b01
strawberry-choco <kangrui.ye@cloudflight.io>
2023-07-07 12:18:52 +0200

    fix rule for ngSrc not found (#160)
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

--- packages/eslint-plugin-angular/src/configs/recommended-html/rules/custom.ts
+++ packages/eslint-plugin-angular/src/configs/recommended-html/rules/custom.ts
@@ -1,6 +1,6 @@
 import {Linter} from 'eslint';
 
-const pluginPrefix = '@cloudflight/angular/template';
+const pluginPrefix = '@cloudflight/angular';
 
 export const customRules: Linter.RulesRecord = {
     [`${pluginPrefix}/img-uses-ngsrc`]: ['error'],
