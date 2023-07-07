69d1346cf9eca8ce4d34c7384a8827a030149bb9
strawberry-choco <kangrui.ye@cloudflight.io>
2023-07-07 12:00:40 +0200

    enforce ngSrc over src attribute for img element (#159)
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

---
+++ packages/eslint-plugin-angular/src/rules/img-uses-ngsrc.ts
@@ -0,0 +1,58 @@
+import type {TmplAstElement} from '@angular-eslint/bundled-angular-compiler';
+import {getTemplateParserServices} from '@angular-eslint/utils';
+import {ESLintUtils} from '@typescript-eslint/utils';
+
+type MessageIds = 'missingAttribute' | 'invalidDoubleSource';
+const ngSrcAttributeName = 'ngSrc';
+
+const createRule = ESLintUtils.RuleCreator(
+    (name) => `https://github.com/cloudflightio/cloudflight-eslint-plugin/tree/main/packages/eslint-plugin-angular/src/rules/${name}.ts`,
+);
+
+export const imgUsesNgsrcRule = createRule<[], MessageIds>({
+    name: 'img-uses-ngsrc',
+    meta: {
+        type: 'suggestion',
+        docs: {
+            description: 'Ensures ngSrc is used instead of src for img elements.',
+            recommended: false,
+        },
+        schema: [],
+        messages: {
+            missingAttribute: 'The attribute [ngSrc] should be used for img elements instead of [src].',
+            invalidDoubleSource: 'Only [ngSrc] should exist on an img element. Delete the [src] attribute.',
+        },
+    },
+    defaultOptions: [],
+    create(context) {
+        const parserServices = getTemplateParserServices(context);
+
+        return {
+            [`Element$1[name=img]`](element: TmplAstElement) {
+                const ngSrcPresent = hasNgSrcAttribute(element);
+                const srcPresent = hasNormalSrcAttribute(element);
+
+                if (!ngSrcPresent) {
+                    context.report({
+                        loc: parserServices.convertNodeSourceSpanToLoc(element.sourceSpan),
+                        messageId: 'missingAttribute',
+                    });
+                }
+                if (ngSrcPresent && srcPresent) {
+                    context.report({
+                        loc: parserServices.convertNodeSourceSpanToLoc(element.sourceSpan),
+                        messageId: 'invalidDoubleSource',
+                    });
+                }
+            },
+        };
+    },
+});
+
+function hasNgSrcAttribute({inputs, attributes}: TmplAstElement): boolean {
+    return [...inputs, ...attributes].some(({name}) => name === ngSrcAttributeName);
+}
+
+function hasNormalSrcAttribute({inputs, attributes}: TmplAstElement): boolean {
+    return [...inputs, ...attributes].some(({name}) => name === 'src');
+}
--- packages/eslint-plugin-angular/src/rules/index.ts
+++ packages/eslint-plugin-angular/src/rules/index.ts
@@ -1,3 +1,6 @@
 import {Rule} from 'eslint';
+import {imgUsesNgsrcRule} from './img-uses-ngsrc';
 
-export const rules: Record<string, Rule.RuleModule> = {};
+export const rules: Record<string, Rule.RuleModule> = {
+    'img-uses-ngsrc': imgUsesNgsrcRule as unknown as Rule.RuleModule,
+};
