46d2aab860cf8254c618864a946a47cb38b4b4ea
Kangrui Johann Ye <kangrui.ye@cloudflight.io>
2024-10-02 15:23:45 +0200

    fix build
    Signed-off-by: Kangrui Johann Ye <kangrui.ye@cloudflight.io>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,11 +10,35 @@ const gitignorePath = resolve(directory, '.gitignore');
 
 export default tseslint.config(
     includeIgnoreFile(gitignorePath),
+    {
+        // these files are temporarily disabled for linting
+        // until we are done with migrating everything to v9
+        ignores: [
+            'packages/eslint-plugin-angular/*',
+            'packages/eslint-plugin-node/*',
+            'packages/eslint-plugin-react/*',
+            'packages/eslint-plugin-vue/*',
+            'packages/eslint-plugin-typescript/src/configs/index.ts',
+            'packages/eslint-plugin-typescript/src/configs/recommended/*',
+            'packages/eslint-plugin-typescript/src/configs/recommended-json/*',
+            'packages/eslint-plugin-typescript/src/configs/recommended-package/*',
+            'packages/eslint-plugin-typescript/src/configs/recommended-typescript/recommended-typescript.config.ts',
+            'packages/eslint-plugin-typescript/src/configs/recommended-typescript/rules/custom.ts',
+            'packages/eslint-plugin-typescript/src/configs/recommended-typescript/rules/format.ts',
+            'packages/eslint-plugin-typescript/src/configs/recommended-typescript/rules/import.ts',
+            'packages/eslint-plugin-typescript/src/configs/recommended-typescript/rules/rxjs.ts',
+            'packages/eslint-plugin-typescript/src/rules/*',
+            'packages/eslint-plugin-typescript/src/util/*',
+            'scripts/*',
+            '.eslintrc.format.js',
+            'eslint.config.mjs',
+        ]
+    },
     ...config,
     {
         languageOptions: {
             parserOptions: {
-                projectService: true,
+                project: ['./packages/*/tsconfig.json', './packages/*/tsconfig.spec.json'],
                 tsconfigRootDir: import.meta.dirname,
             },
         },
--- packages/eslint-plugin-typescript/src/index.ts
+++ packages/eslint-plugin-typescript/src/index.ts
@@ -3,15 +3,21 @@ import tseslint from 'typescript-eslint';
 import {eslintRules} from './configs/recommended-typescript/rules/eslint';
 import {typescriptEslintRules} from './configs/recommended-typescript/rules/typescript-eslint';
 
-export const config = tseslint.config({
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    ignores: ['jest.config*.ts'],
-    extends: [
-        pluginJs.configs.recommended,
-        ...tseslint.configs.recommendedTypeChecked,
-    ],
-    rules: {
-        ...eslintRules,
-        ...typescriptEslintRules,
-    }
-});
+export const config = tseslint.config(
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        ignores: ['jest.config*.ts'],
+        extends: [
+            pluginJs.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
+        ],
+        rules: {
+            ...eslintRules,
+            ...typescriptEslintRules,
+        }
+    },
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        extends: [tseslint.configs.disableTypeChecked],
+    },
+);
