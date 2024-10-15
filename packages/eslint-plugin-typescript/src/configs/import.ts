d1bfdaec214d8d61d5cf229a3d5da1e179448ffb
Kangrui Johann Ye <kangrui.ye@cloudflight.io>
2024-10-15 09:52:57 +0200

    disable import-x/namespace rule
    Signed-off-by: Kangrui Johann Ye <kangrui.ye@cloudflight.io>

--- packages/eslint-plugin-typescript/src/configs/import.ts
+++ packages/eslint-plugin-typescript/src/configs/import.ts
@@ -3,6 +3,8 @@ import {TSESLint} from '@typescript-eslint/utils';
 const pluginPrefix = 'import-x';
 
 export const importEslintRules: TSESLint.Linter.RulesRecord = {
+    // computationally expensive and checked by typescript already
+    [`${pluginPrefix}/namespace`]: 'off',
     [`${pluginPrefix}/first`]: ['error'],
     [`${pluginPrefix}/no-absolute-path`]: ['error'],
     [`${pluginPrefix}/no-cycle`]: ['error', {ignoreExternal: true}],
